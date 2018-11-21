2cad0fc253aeda27124e14ceb0a5e41da3ad6667
Olessia Potapova <olessia.potapova@gmail.com>
2018-11-21 15:08:47 -0300

    added props to tweet

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import moment from 'moment';
 
-function Tweet() {
+function Tweet({tweet}) {
     return(
         <div className='tweet'>
-            <Avatar/> 
+            <Avatar hash={tweet.gravatar}/> 
             <div className='content'>
-                <NameWithHandle/><Time/>
-                <Message/> 
+                <NameWithHandle author={tweet.author}/>
+                <Time time={tweet.timestamp}/>
+                <Message message={tweet.message}/> 
                 <div className='buttons'>
                     <ReplyButton />
-                    <RetweetButton />
-                    <LikeButton />
+                    <RetweetButton count={tweet.retweets} />
+                    <LikeButton count={tweet.likes}/>
                     <MoreOptionsButton />
                 </div>
             </div>
@@ -20,53 +22,86 @@ function Tweet() {
     );
 }
 
-function Avatar() {
+function Avatar({hash}) {
+    var url=`https://images.pexels.com/photos/${hash}`
     return(
         <img
-        src="https://www.gravatar.com/avatar/9ab982de687898f228e8cbe183f3a362"
+        src={url}
         className='avatar'
         alt='avatar'/>
     );
 }
 
-function NameWithHandle() {
+function NameWithHandle({author}) {
+    const {name, handle} = author
     return(
         <span className='name-with-handle'>
-            <span className="name">Your Name</span>
-            <span className="handle">@yourhandle</span>
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
         </span>
     );
 }
 
-function Message() {
+function Message({message}) {
     return(
         <div className="message">
-            Message body goes here
+           {message}
         </div>
     )
 } 
 
-const Time = () => (
-    <span className="time"> 3h ago</span>
-);
+const Time = ({time}) => {
+    var timeString = moment(time).fromNow();
+    return(
+        <span className="time">{timeString}</span>
+    )
+};
 
 const ReplyButton = () => (
     <i className="fa fa-reply reply-button"/>
 );
 
-const RetweetButton = () => (
-    <i class="fa fa-retweet retweet-button"></i>
-);
+const RetweetButton = ({count}) => {
+    return(
+    <span className="retweet-button">
+        <i className="fa fa-retweet"/>
+        {count > 0 && 
+            <span className="retweet-count">
+            {count}
+        </span>}
+    </span>
+    );
+};
 
-const LikeButton = () => (
-    <i className="fa fa-heart like-button"/>
-);
+const LikeButton = ({count}) => {
+    return(
+    <span className="like-button">
+        <i className="fa fa-heart"/>
+        {count > 0 && 
+        <span className="like-count">
+            {count}
+        </span>}
+    </span>
+    );
+};
 
 const MoreOptionsButton = () => (
     <i className="fa fa-ellipsis-h more-options-button"/>
 );
 
+const testTweet = {
+    gravatar:'1571724/pexels-photo-1571724.jpeg?auto=compress&cs=tinysrgb&h=350',
+    message:"robots dance on the tamborine",
+    author: {
+        name: 'Oli',
+        handle: "oleeeeeeessia"
+    },
+    timestamp: 'Wed Nov 21 2018 14:29:44 GMT-0300',
+    retweets: 3,
+    likes:234
+}
+
 ReactDOM.render(
-    <Tweet/>,
+    <Tweet tweet={testTweet}/>,
     document.querySelector('#root')
 );
